413c935f299ecfdb3c1480cbf9b9189f361a87dd
nayazella <kacangnutella875@gmail.com>
2026-02-03 14:35:46 +0800

    commit pertama

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+  if(session_status() == PHP_SESSION_NONE){
+    session_start();
+  }
+  $koneksi = mysqli_connect("localhost","root","","perpus_digital");
+  if (mysqli_connect_errno()){
+    echo "Koneksi database gagal : " . mysqli_connect_error();
+  }
+?>
